299fabb71dd070bf14d9a1b9ef14be039a20cf74
Kitheka <oulxxtg@gmail.com>
2025-08-20 16:29:55 +0300

    Navigation bar

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,14 @@
-import { Stack } from "expo-router";
+import {Stack, Tabs} from "expo-router";
+import React from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+  <Stack>
+    <Stack.Screen
+    name="(tabs)"
+    options={{headerShown: false}}
+    />
+
+  </Stack>
+  )
 }
