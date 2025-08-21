33e6da9b10c398ed1ac202e25049b5c848e3fa05
Kitheka <oulxxtg@gmail.com>
2025-08-21 11:31:33 +0300

    updated navigation bar

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,17 +1,23 @@
-import { Text, View } from "react-native";
-import {Link} from "expo-router";
+import { Text, View, Image, Dimensions } from "react-native";
+import { Link } from "expo-router";
+
+const { width, height } = Dimensions.get("window");
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-	    backgroundColor: "white"
-      }}
-    >
-      <Image source={require("../assets/images/logo.png")} style={{width: width/2, height: height/2}} contentFit="contain"/>
-    </View>
-  );
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "black",
+            }}
+        >
+            <Image
+                source={require("../../assets/images/logo.png")}
+                style={{ width: width / 2, height: height / 2 }}
+                resizeMode="contain"
+            />
+        </View>
+    );
 }
