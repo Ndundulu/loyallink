62187acb3b2e889ffc4cd9d7483fc2895683ca7c
QuantumSnow0 <bmuthuri93@gmail.com>
2025-08-23 12:03:09 +0300

    added signout button

--- app/(tabs)/Home.jsx
+++ app/(tabs)/Home.jsx
@@ -2,9 +2,11 @@ import { Image, View } from 'react-native';
 import React from 'react';
 import Greeting from '../components/greeting';
 import SearchBar from '../components/searchBar';
+import {SignOutButton} from '../Components/SignOutButton'
 const HomeScreen = () => {
     return (
         <View style={{justifyContent:'center', alignItems:'center'}}>
+	<SignOutButton/>
             <Image source={require('../../assets/images/logo.png')}
                 style={{width: 350, height: 350,}}
             />
